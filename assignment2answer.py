310db2767eeec8c38866467239231b31e361b7ea
gautamsharma104 <97290530+gautamsharma104@users.noreply.github.com>
2022-01-26 17:16:02 +0530

    Add files via upload

---
+++ assignment2answer.py
@@ -0,0 +1,67 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:\Users\Shree\Desktop\assignment2.py ===============
+question1
+1a
+36
+
+1b
+['paython', 'is', 'a', 'case', 'sencitive', 'language']
+['language', 'sencitive', 'case', 'a', 'is', 'paython']
+language sencitive case a is paython
+
+1c
+['paython', 'is', 'a', 'case', 'sencitive', 'language']
+['a', 'case', 'sencitive']
+a case sencitive
+
+1d
+paython is object oriented language
+
+1e
+10
+
+1f
+['paython', 'is', 'a', 'case', 'sencitive', 'language']
+paythonisacasesencitivelanguage
+
+question2
+hay GAUTAM SHARMA here!
+my SID is 21104056
+i am from  ELECRICAL ENGINEERING and my CGPA is 8.8
+
+question3
+a
+8
+
+b
+58
+
+c
+50
+
+d
+224
+40
+
+e
+14
+0
+
+question4
+enter the first number 45
+enter the second number 98
+enter the third number 12
+greater number is 98
+
+question5
+enter a string :what is your name
+yes ,name is in the string
+
+question6
+enter the first side: 32
+enter the second side: 12
+enter the third side: 40
+yes given lengths form a triangle
+>>> 
